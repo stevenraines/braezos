5e18a7ee1754a49a41ded817712e357eae9bb24e
Steven Raines <raines@pointman.com>
2021-02-06 11:30:59 -0500

    added location detail component

---
+++ src/components/LocationDetail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="location-detail">hi</div>
+</template>
+<script>
+export default {
+  name: "LocationDetail",
+  data: function() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.location-detail {
+  height: 400px;
+  width: 300px;
+  border: 1px solid #000000;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
     <Map />
+    <LocationDetail />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Map from "@/components/Map.vue";
-
+import LocationDetail from "@/components/LocationDetail.vue";
 export default {
   name: "Home",
   components: {
     Map,
+    LocationDetail,
   },
 };
 </script>
