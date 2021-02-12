192c5846aeaa4e3555dbcbe8a7c9d7e7389c5fea
Steven Raines <raines@pointman.com>
2021-02-12 15:10:35 -0500

    fixed issue loading w/ no preexisting state

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -7,27 +7,6 @@
         </div>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col cols="12">
-        <table>
-          <tr>
-            <td><v-btn x-small v-on:click="zoomSVG(10)">+</v-btn></td>
-            <td><v-btn x-small v-on:click="panSVGV(10)">Up</v-btn></td>
-            <td><v-btn x-small v-on:click="zoomSVG(-10)">-</v-btn></td>
-          </tr>
-          <tr>
-            <td><v-btn x-small v-on:click="panSVGH(10)">Left</v-btn></td>
-            <td><v-btn x-small v-on:click="resetSVG()">%</v-btn></td>
-            <td><v-btn x-small v-on:click="panSVGH(-10)">Right</v-btn></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td><v-btn x-small v-on:click="panSVGV(-10)">Down</v-btn></td>
-            <td></td>
-          </tr>
-        </table>
-      </v-col>
-    </v-row>
   </v-container>
 </template>
 
@@ -56,32 +35,25 @@ export default {
     dynamicComponent: function() {
       return {
         template: `<div>${this.renderedMap}</div>`,
-        methods: {
-          someAction() {
-            console.log('Action!');
-          },
-        },
+        methods: {},
       };
     },
   },
   async created() {
     EventBus.$on('click', this.handleGlobalEvent);
-
+    EventBus.$on('setupComplete', this.renderMap);
     await this.renderMap();
   },
   async beforeUpdate() {},
   methods: {
     renderMap: async function() {
-      console.log('rendermap', this.$store.state);
       if (!this.$store.state.player.location) return;
 
       renderer.init(this.$store.state.places, this.height, this.width);
-
       this.renderedMap = await renderer.renderMap(
         this.$store.state.player.location
       );
     },
-
     handleGlobalEvent: async function(message) {
       if (message.event == 'click' && message.source == 'map') {
         await this.$store.dispatch(
@@ -90,53 +62,6 @@ export default {
         );
         await this.renderMap();
       }
-    },
-    resetSVG: function() {
-      this.mapSVG.postMessage(
-        {
-          event: 'resetSVG',
-          elementId: 'map',
-          height: this.height,
-          width: this.width,
-        },
-        '*'
-      );
-    },
-    panSVGV: function(amount) {
-      this.mapSVG.postMessage(
-        {
-          event: 'panSVGV',
-          elementId: 'map',
-          amount: amount,
-          height: this.height,
-          width: this.width,
-        },
-        '*'
-      );
-    },
-    panSVGH: function(amount) {
-      this.mapSVG.postMessage(
-        {
-          event: 'panSVGH',
-          elementId: 'map',
-          amount: amount,
-          height: this.height,
-          width: this.width,
-        },
-        '*'
-      );
-    },
-    zoomSVG: function(amount) {
-      this.mapSVG.postMessage(
-        {
-          event: 'zoomSVG',
-          elementId: 'map',
-          amount: amount,
-          height: this.height,
-          width: this.width,
-        },
-        '*'
-      );
     },
   },
 };
